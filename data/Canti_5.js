e1b94095d3d0d3b4dc2789f8e5f6db128ec1dac1
wellicagp <wellicagp@gmail.com>
2020-05-31 00:17:08 +0700

    this a new commit

---
+++ data/Canti_5.js
@@ -0,0 +1,8 @@
+var json_Canti_5 = {
+"type": "FeatureCollection",
+"name": "Canti_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 105.592855974237366, -5.796611048476562 ] } }
+]
+}
